5519bc52dc0a7137f93f0a465989d853cf47cd02
Hugo MARTIN <hugo.martin@done.fr>
2025-05-06 15:46:29 +0200

    feat: async hero fetching and message queue

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroDetailsComponent} from "../hero-details/hero-details.component";
+import { HeroService } from '../hero.service';
+import { MessageService } from "../message.service";
 
 import { HEROES } from "../mock-heroes";
 import { Hero } from "../hero";
@@ -13,11 +15,22 @@ import { Hero } from "../hero";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-    heroes: Hero[] = HEROES;
+    heroes: Hero[] = [];
     selectedHero?: Hero;
+
+    constructor(private heroService: HeroService, private messageService: MessageService){}
+
+    ngOnInit(){
+        this.fetchHeroes();
+    }
+
+    fetchHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero): void{
         this.selectedHero = hero;
-        console.log(hero);
+        this.messageService.add(`HeroesComponent : ${hero.name} selected`);
     }
 }
