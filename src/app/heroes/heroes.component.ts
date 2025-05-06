a821395efd6830448e1b367cff4521420abcae1e
Hugo MARTIN <hugo.martin@done.fr>
2025-05-06 15:02:42 +0200

    feat: heroes list with details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { HEROES } from "../mock-heroes";
 import { Hero } from "../hero";
 
 @Component({
@@ -10,8 +12,11 @@ import { Hero } from "../hero";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-    hero: Hero = {
-        id: 1,
-        name: "Vent-tempête"
-    };
+    heroes: Hero[] = HEROES;
+    selectedHero?: Hero;
+
+    onSelect(hero: Hero): void{
+        this.selectedHero = hero;
+        console.log(hero);
+    }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Cool' },
+  { id: 13, name: 'Bazelgeuse' },
+  { id: 14, name: 'Rapide-man' },
+  { id: 15, name: 'Aimant-man' },
+  { id: 16, name: 'Caoutchouc-man' },
+  { id: 17, name: 'Dyn-ami' },
+  { id: 18, name: 'Dr. QI' },
+  { id: 19, name: 'Lave' },
+  { id: 20, name: 'Tornade' }
+];
