79c9aa2d7a74f9e021ab06c7d69155f012208b8c
Hugo MARTIN <hugo.martin@done.fr>
2025-05-06 12:24:26 +0200

    feat: hero infos and tw-way binding

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from "../hero";
+
+@Component({
+  selector: 'app-heroes',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+    hero: Hero = {
+        id: 1,
+        name: "Vent-tempête"
+    };
+}
