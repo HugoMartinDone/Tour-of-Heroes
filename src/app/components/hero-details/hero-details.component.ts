f7e9e1f94a705bc03ee5dfb6e1337c220a9327df
Hugo MARTIN <hugo.martin@done.fr>
2025-05-09 15:20:27 +0200

    feat: FormGroup/Contol for hero-details

--- src/app/components/hero-details/hero-details.component.ts
+++ src/app/components/hero-details/hero-details.component.ts
@@ -1,20 +1,27 @@
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule, Location } from "@angular/common";
-import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HeroService } from '../../services/hero.service';
 
 import { Hero } from "../../models/hero.model";
+import { HeroClass } from "../../models/hero-class.enum";
 
 @Component({
   selector: 'app-hero-details',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './hero-details.component.html',
   styleUrl: './hero-details.component.css'
 })
 export class HeroDetailsComponent implements OnInit {
     @Input() hero?: Hero;
+    form: FormGroup = new FormGroup({
+        name: new FormControl("", Validators.required),
+        class: new FormControl("", Validators.required)
+    });
+    classes = Object.values(HeroClass).filter(x => typeof x !== "number");
 
     constructor(
       private route: ActivatedRoute,
@@ -29,7 +36,13 @@ export class HeroDetailsComponent implements OnInit {
     fetchHero(): void{
         const id = Number(this.route.snapshot.paramMap.get('id'));
         this.heroService.getHero(id)
-            .subscribe(hero => this.hero = hero);
+            .subscribe(hero => {
+                this.hero = hero;
+                this.form.setValue({
+                    name: hero.name,
+                    class: hero.class
+                });
+            });
     }
 
     save(): void{
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -8,15 +8,15 @@ import { Hero } from '../models/hero.model';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const heroes = [
-            { id: 12, name: 'Dr. Cool', class: "TEST" },
-            { id: 13, name: 'Bazelgeuse', class: "TEST" },
-            { id: 14, name: 'Rapide-man', class: "TEST" },
-            { id: 15, name: 'Aimant-man', class: "TEST" },
-            { id: 16, name: 'Caoutchouc-man', class: "TEST" },
-            { id: 17, name: 'Dyn-ami', class: "TEST" },
-            { id: 18, name: 'Dr. QI', class: "TEST" },
-            { id: 19, name: 'Lave', class: "TEST" },
-            { id: 20, name: 'Tornade', class: "TEST" }
+            { id: 12, name: 'Dr. Cool', class: "WARRIOR" },
+            { id: 13, name: 'Bazelgeuse', class: "RANGED" },
+            { id: 14, name: 'Rapide-man', class: "SUPPORT" },
+            { id: 15, name: 'Aimant-man', class: "TANK" },
+            { id: 16, name: 'Caoutchouc-man', class: "TANK" },
+            { id: 17, name: 'Dyn-ami', class: "WARRIOR" },
+            { id: 18, name: 'Dr. QI', class: "SUPPORT" },
+            { id: 19, name: 'Lave', class: "WARRIOR" },
+            { id: 20, name: 'Tornade', class: "RANGED" }
         ];
         return {heroes};
     }
